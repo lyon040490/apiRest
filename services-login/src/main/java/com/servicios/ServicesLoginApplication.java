edfa1508a2a1c55f8312d31e268e9eeee50b2abf
leonardovg <leonardovg@192.168.100.139>
2020-09-21 10:19:47 -0500

    Cimmit inicial - carga de todos los archivos

---
+++ services-login/src/main/java/com/servicios/ServicesLoginApplication.java
@@ -0,0 +1,13 @@
+package com.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesLoginApplication.class, args);
+	}
+
+}
