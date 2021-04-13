a236a2f3efe76ada342e84499f3b1a4dc33d04d4
Leonardo Velazquez Garcia <leonardovg@MacBook-Pro-de-Leonardo.local>
2021-04-12 19:54:33 -0500

    Commit de cambios a DEV

--- services-login/src/main/java/com/servicios/ServicesLoginApplication.java
+++ services-login/src/main/java/com/servicios/ServicesLoginApplication.java
@@ -2,7 +2,7 @@ package com.servicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**  Commit Rama DEV **/
 @SpringBootApplication
 public class ServicesLoginApplication {
 
